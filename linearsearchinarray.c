4709bbdf71c3b107156250937f5bb6db8e010349
RahulPatel <rahul.patelcse2024@indoreinstitute.com>
2025-09-22 11:14:00 +0530

    Update linearsearchinarray.c

--- linearsearchinarray.c
+++ linearsearchinarray.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 void main(){
-    int arr[5]={1,2,3,4,5},n,i;
+    int arr[5]={1,2,3,4,5},n,i,flag=0;
     printf("Enter a element to search: ");
     scanf("%d",&n);
     for(i=0;i<5;i++){
         if(arr[i]==n){
             printf("Element found at index %d",i);
+            flag++;
             break;
         }
     }
+    else printf("Element not found!");
 }
